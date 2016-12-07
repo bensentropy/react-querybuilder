cece0f39c319978d99558ae89536933cc3d7ec3c
Ben Olsen <bentho@gmail.com>
2016-12-07 10:42:21 -0800

    fix loaders 7

--- dist/demo/demo.bundle.js
+++ dist/demo/demo.bundle.js
@@ -9477,7 +9477,6 @@ module.exports = __webpack_require__(130);
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.QueryBuilder = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -9505,7 +9504,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var QueryBuilder = exports.QueryBuilder = function (_React$Component) {
+var QueryBuilder = function (_React$Component) {
     _inherits(QueryBuilder, _React$Component);
 
     _createClass(QueryBuilder, null, [{
@@ -9860,6 +9859,8 @@ var QueryBuilder = exports.QueryBuilder = function (_React$Component) {
 
     return QueryBuilder;
 }(_react2.default.Component);
+
+exports.default = QueryBuilder;
 
 /***/ },
 /* 85 */
--- dist/index.js
+++ dist/index.js
@@ -1953,7 +1953,6 @@ module.exports = ReactPropTypesSecret;
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.QueryBuilder = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -1981,7 +1980,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var QueryBuilder = exports.QueryBuilder = function (_React$Component) {
+var QueryBuilder = function (_React$Component) {
     _inherits(QueryBuilder, _React$Component);
 
     _createClass(QueryBuilder, null, [{
@@ -2336,6 +2335,8 @@ var QueryBuilder = exports.QueryBuilder = function (_React$Component) {
 
     return QueryBuilder;
 }(_react2.default.Component);
+
+exports.default = QueryBuilder;
 
 /***/ },
 /* 21 */
